c9f1edb012581f7b28ba6249079cac9d012a4934
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-13 13:57:02 +0530

    css

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -13,10 +13,26 @@ import { useRecoilState } from 'recoil';
 import { loadingStatus } from '../../Recoil';
 import LoadingScreen from '../../Component/LoadingScreen/LoadingScreen';
 import OrderTable from '../../Component/OrderTable/OrderTable';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend ,CategoryScale ,LinearScale,PointElement,LineElement} from "chart.js";
+import { Doughnut,Line } from "react-chartjs-2";
 
+ChartJS.register(ArcElement, Tooltip, Legend,CategoryScale,LinearScale,PointElement,LineElement);
 
 function DashBoard() {
   const [isLoading, SetIsloading] = useRecoilState(loadingStatus);
+  // Sample sales data
+  const salesData = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "Sales",
+        data: [100, 600, 200, 800, 500, 100], // Example sales data for each month
+        fill: true,
+        borderColor: "#5ba6a2", // Line color
+        tension: 0.4 // Smoothing of the line
+      }
+    ]
+  };
 
   return (
     <div className={style.main}>
@@ -28,8 +44,8 @@ function DashBoard() {
       <Header/>
       <BasicCard/>
       <div className={style.chart}>
-      <Combining/>
-        <PieArcLabel/>
+      <Line data={salesData} />
+        {/* <PieArcLabel/> */}
         </div>
  
       <br/>
